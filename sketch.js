76f957f0212e4475ea9268305fd74e1f35e9de85
divinejoshua <divineuche399@gmail.com>
2023-06-11 11:04:29 +0100

    Made changes to imagess

--- sketch.js
+++ sketch.js
@@ -3,78 +3,81 @@
     console.clear()
 
     let video = null
-    let poses = []
-    let noseX = null
-    let noseY = null
+    let predictions = [];
 
     // Create a new image element
-    var redDotImg = new Image();
-    redDotImg.src = 'images/reddot.png';
-
-     // Create a new image element
-    var clownHatImg = new Image();
-    clownHatImg.src = 'images/clown-hat.png';
+    var cursorImg = new Image();
+    cursorImg.src = 'images/cursor.png';
 
 
     // On set up 
     function setup() {
-        createCanvas(1200,700);
+        createCanvas(600,500);
         video = createCapture(VIDEO)
-        video.size(1200, 700)
+        video.size(600, 500)
 
-        let poseFinder = ml5.poseNet(video); 
-        // Listen to new 'pose' events
-        poseFinder.on('pose', (results) => {
-            poses = results;
-          });
+       // Create a new instance of Handpose
+        let handpose = ml5.handpose(video, modelLoaded);
+
+        
+       // Set up a callback function to handle the results
+        handpose.on("predict", gotPredictions);
                     
         video.hide()
     }
 
+
+    // Callback function to handle hand detection results
+function gotPredictions(results) {
+    predictions = results;
+}
+
+
+// Callback function when Handpose model is loaded
+function modelLoaded() {
+    console.log('Model loaded!');
+}
+
+
+
     // On camera 
     function draw() {
         background(220);
-        image(video,0,0,1200,700)
-        redDotAnimation() //Red dot animation
+        image(video,0,0,600,500)
+      
+        cursorAnimation() //Cursor animation
         
     }
-
 
 
     //===================Animations ====================
 
     //This animation is a red dot on the nose
-    function redDotAnimation(){
-        if(poses.length > 0) {
-            noseX = poses[0].pose.nose.x
-            noseY = poses[0].pose.nose.y
-            redDotImg.style.position = 'absolute';
-            redDotImg.style.left = noseX-17 + 'px';
-            redDotImg.style.top = noseY + 50 + 'px';
-            redDotImg.style.width = 50 + 'px';
-            redDotImg.style.height = 50 + 'px';
+    function cursorAnimation(){
 
-            // Add the red dot to the dom 
-            document.body.appendChild(redDotImg);
-             
+        // Check if hand predictions have been made
+        if (predictions.length > 0) {
+            // Locate the index finger
+            let hand = predictions[0];
+            let indexFinger = hand.annotations.indexFinger[3];
+
+            let indexFingerX = indexFinger[0];
+            let indexFingerY = indexFinger[1];
+
+            // Display the index finger position
+
+            // push()
+            cursorImg.style.position = 'absolute';
+            cursorImg.style.left = indexFingerX + 'px';
+            cursorImg.style.top = indexFingerY + 'px';
+            cursorImg.style.width = 50 + 'px';
+            cursorImg.style.height = 50 + 'px';
+            // pop()
+
+
+            
+            // fill(255, 0, 0);
+            // ellipse(indexFingerX, indexFingerY, 10, 10);
         }
-
-    }
-
-    //This animation is for the clown hat
-    function clownHatAnimation(){
-        if(poses.length > 0) {
-            noseX = poses[0].pose.nose.x
-            noseY = poses[0].pose.nose.y
-            clownHatImg.style.position = 'absolute';
-            clownHatImg.style.left = noseX-200 + 'px';
-            clownHatImg.style.top = noseY - 350 + 'px';
-            clownHatImg.style.width = 400 + 'px';
-            clownHatImg.style.height = 400 + 'px';
-
-            // Add the red dot to the dom 
-            document.body.appendChild(clownHatImg);
              
-        }
-
     }
